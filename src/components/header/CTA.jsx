c3672bf800a8bd89c12322fd4ec9c5d9ef8fa8ff
Pranjal Bansal <pranban12@gmail.com>
2023-12-23 14:15:29 +0530

    third change

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Pranjal_Resume from '../../Assets/Pranjal_Resume.pdf'
+import Pranjal_Bansal_Resume from '../../Assets/Pranjal_Bansal_Resume.pdf'
 
 export const CTA = () => {
   return (
     <div className='cta'>
-      <a href={Pranjal_Resume} download className='btn'> Download CV</a>
+      <a href={Pranjal_Bansal_Resume} download className='btn'> Download CV</a>
       <a href="#contact" className='btn btn-primary'>Lets Talk</a>
     </div>
   )
